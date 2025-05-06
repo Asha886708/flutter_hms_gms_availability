910e9822a20496ada6195194edbaebbe90c651d8
Asha886708 <ashaanand.a07@gmail.com>
2025-05-06 15:14:57 +0530

    Update plugin to use Flutter Embedding V2

--- android/src/main/kotlin/com/chimou/flutter_hms_gms_availability/FlutterHmsGmsAvailabilityPlugin.kt
+++ android/src/main/kotlin/com/chimou/flutter_hms_gms_availability/FlutterHmsGmsAvailabilityPlugin.kt
@@ -4,50 +4,21 @@ import androidx.annotation.NonNull
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler
-import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
-import android.content.Context
-import com.huawei.hms.api.ConnectionResult
-import com.huawei.hms.api.HuaweiApiAvailability
-import com.google.android.gms.common.GoogleApiAvailability
 
-/** FlutterHmsGmsAvailabilityPlugin */
-public class FlutterHmsGmsAvailabilityPlugin : FlutterPlugin, MethodCallHandler {
+class FlutterHmsGmsAvailabilityPlugin : FlutterPlugin, MethodChannel.MethodCallHandler {
 
- var context: Context? = null
+    private lateinit var channel: MethodChannel
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    val channel = MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "flutter_hms_gms_availability")
-    context = flutterPluginBinding.applicationContext
-    channel.setMethodCallHandler(this)
-  }
-
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    // do nothing
-  }
-
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "isHmsAvailable") {
-      result.success(isHmsAvailable())
-    } else if (call.method == "isGmsAvailable") {
-      result.success(isGmsAvailable())
-    } else {
-      result.notImplemented()
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "flutter_hms_gms_availability")
+        channel.setMethodCallHandler(this)
     }
-  }
 
-  private fun isHmsAvailable(): Boolean {
-    val result = context?.let { HuaweiApiAvailability.getInstance().isHuaweiMobileServicesAvailable(it) }
-    val isAvailable = ConnectionResult.SUCCESS == result
-    
-    return isAvailable
-  }
+    override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
+        result.notImplemented() // Implement your method handling here if needed
+    }
 
-  private fun isGmsAvailable(): Boolean {   
-    val result: Int? = context?.let { GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(it) }
-    val isAvailable = com.google.android.gms.common.ConnectionResult.SUCCESS === result
-    
-    return isAvailable
-  }
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+    }
 }
